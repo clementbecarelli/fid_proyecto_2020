669905c5fd32584dfa25979460c6b5f6d9a64e43
Alfredo Fuentes <afuentes2510@gmail.com>
2021-01-01 13:55:05 +0100

    Estructura inicial

---
+++ VoiceGenderRecognition.R
@@ -0,0 +1,8 @@
+rawData<-read.csv("data/voice.csv")
+
+#Seleccion de caracteristicas mas importantes
+#Visualiozacion de datos Hombre/mujer en relacion a esas caracteristicas
+
+#Clustering o algun aprendizaje no supervisado
+
+#Algoritmos de aprendizaje supervisado (Por ejemplo con todas y con solo las mas importantes para comparar)
